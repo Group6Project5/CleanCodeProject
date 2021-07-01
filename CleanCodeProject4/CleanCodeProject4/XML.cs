8802f755f6337bd31fa8851cf5a3797581dab99e
HassanAlgadah <h--san@hotmail.com>
2021-07-01 19:02:09 +0300

    add refactoring

--- CleanCodeProject4/CleanCodeProject4/XML.cs
+++ CleanCodeProject4/CleanCodeProject4/XML.cs
@@ -5,53 +5,41 @@ namespace CleanCodeProject4
 {
     public class XML
     {
+        private XmlElement MembersElement;
+        private XmlDocument xmldoc;
         public void CreateXmlFile()
         {
-            XmlDocument xmldoc = new XmlDocument();
+            xmldoc = new XmlDocument();
             XmlElement root = xmldoc.CreateElement("Team");
             xmldoc.AppendChild(root);
-
-            XmlElement element1 = xmldoc.CreateElement("Members");
-            root.AppendChild(element1);
-
-            XmlElement element2 = xmldoc.CreateElement("Member");
-            XmlText text2 = xmldoc.CreateTextNode("Batool Alghamdi , Computer Science");
-            element1.AppendChild(element2);
-            element2.AppendChild(text2);
-
-            XmlElement element3 = xmldoc.CreateElement("Member");
-            XmlText text3 = xmldoc.CreateTextNode("Rawabi Alshaduki , Software Engineer");
-            element1.AppendChild(element3);
-            element3.AppendChild(text3);
-
-            XmlElement element4 = xmldoc.CreateElement("Member");
-            XmlText text4 = xmldoc.CreateTextNode("Ahmed Almutairi, Computer science ");
-            element1.AppendChild(element4);
-            element4.AppendChild(text4);
-
-            XmlElement element5 = xmldoc.CreateElement("Member");
-            XmlText text5 = xmldoc.CreateTextNode("Thamer Mashni, Computer science ");
-            element1.AppendChild(element5);
-            element5.AppendChild(text5);
-
-            XmlElement element6 = xmldoc.CreateElement("Member");
-            XmlText text6 = xmldoc.CreateTextNode("Hassam Algudah, Software Engineer ");
-            element1.AppendChild(element6);
-            element6.AppendChild(text6);
-
-            xmldoc.Save(@"/Users/thamerasiri/Desktop/Team.xml");
+            MembersElement = xmldoc.CreateElement("Members");
+            root.AppendChild(MembersElement);
+            AddMember("Hassan Algadah , Software Engineering");
+            AddMember("Rawabi Alshaduki , Software Engineering");
+            AddMember("Batool Alghamdi , Computer Science");
+            AddMember("Ahmed Almutairi , Computer Science");
+            AddMember("Thamer Mashni , Computer Science");
+            xmldoc.Save(@"../../../Team.xml");
             Console.WriteLine(xmldoc.InnerXml);
+        }
+        public void AddMember(string memberinfo)
+        {
+            XmlElement memberElement = xmldoc.CreateElement("Member");
+            XmlText memberNode = xmldoc.CreateTextNode(memberinfo);
+            MembersElement.AppendChild(memberElement);
+            memberElement.AppendChild(memberNode);
         }
         public void ReadFile()
         {
             XmlDocument xmldoc = new XmlDocument();
-            xmldoc.Load(@"/Users/thamerasiri/Desktop/Team.xml");
-            foreach (XmlNode node in xmldoc.DocumentElement.ChildNodes)
+            xmldoc.Load(@"../../../Team.xml");
+            var membersTag = xmldoc.DocumentElement.ChildNodes[0]; 
+            
+            foreach (XmlNode node in membersTag)
             {
                 string element = node.Name;
-                Console.WriteLine(element + ": ");
                 string text = node.InnerText;
-                Console.WriteLine(text);
+                Console.WriteLine(element + ": "+text);
             }
         }
     }
